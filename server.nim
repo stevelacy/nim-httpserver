bcfbee1e4a5171b4a28acfc9a22637c236771058
Steve Lacy <me@slacy.me>
2016-01-28 23:23:10 -0800

    indent

--- server.nim
+++ server.nim
@@ -1,6 +1,7 @@
 import asynchttpserver, asyncdispatch
 
 var server = newAsyncHttpServer()
-proc cb(req: Request) {.async.} = await req.respond(Http200, "Nim server")
+proc cb(req: Request) {.async.} =
+  await req.respond(Http200, "Nim server")
 
 waitFor server.serve(Port(8080), cb)
