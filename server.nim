4911a95a8075c8477d40bc1f396180069c93a660
Steve Lacy <me@slacy.me>
2016-01-29 00:48:18 -0800

    add Routes

--- server.nim
+++ server.nim
@@ -1,7 +1,31 @@
 import asynchttpserver, asyncdispatch
 
+
+type
+  Route = ref object of RootObj
+    path: string
+    body: string
+
+var
+  Routes: seq[Route] = @[]
+
+Routes.add(Route(
+  body: "home",
+  path: "/"
+))
+Routes.add(Route(body: "test", path: "/test"))
+
+# for k, v in Routes:
+#   echo v.body, v.path
+
+
 var server = newAsyncHttpServer()
 proc cb(req: Request) {.async.} =
-  await req.respond(Http200, "Nim server")
+  echo req.reqMethod, " ", req.url.path
+  for k, v in Routes:
+    if v.path != req.url.path: continue
+    await req.respond(Http200, v.body)
+
+  await req.respond(Http404, "404 - not found")
 
 waitFor server.serve(Port(8080), cb)
